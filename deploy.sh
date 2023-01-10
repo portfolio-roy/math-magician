a2deee970e281d1bd75676266ae1a4be5e7dd42d
Utsargo Roy <royutsargo@gmail.com>
2023-01-10 11:52:40 +0600

    Deploy test

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+npm run build
+cp -rv build/* docs/
+for fl in docs/static/js/*.js; do  
+     sed -i '1s;^;/* eslint-disable */\n;' $fl
+     done
+for fl in docs/static/css/*.css; do  
+     sed -i '1s;^;/* stylelint-disable */\n;' $fl
+     done
+npx eslint . --fix && npx stylelint "**/*.{css,scss}" --fix --custom-syntax postcss-scss
+git add *
+read -p 'Commit Message: ' commitMessage
+git commit -m "$commitMessage"
+read -p 'Branch: ' branchName
+git push -u origin $branchName
