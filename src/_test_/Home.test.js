7b048c574298964791d2376bc159ddc58ed5bec7
Utsargo Roy <royutsargo@gmail.com>
2023-01-26 15:12:25 +0600

    Add tests

--- src/_test_/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Home from '../components/Home';
-
-it('matches snapshot', () => {
-  const tree = renderer.create(<Home />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
